d4b9991cdb0ef66f8dd094405a918cf603454b7e
rabbyhasan <rabbyhasan4594@gmail.com>
2023-05-03 04:13:48 +0600

    logout added

--- src/NavigationBar/NavigationBar.jsx
+++ src/NavigationBar/NavigationBar.jsx
@@ -3,10 +3,17 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-
+import { AuthContext } from '../providers/AuthProvider';
+import { Button } from 'react-bootstrap';
 
 const NavigationBar = () => {
-    
+    const { user, logOut, loading } = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOut()
+            .then()
+            .catch(error => console.log(error));
+    }
     return (
         <Navbar bg="light" expand="lg">
             <Container>
@@ -21,12 +28,14 @@ const NavigationBar = () => {
 
                     <Nav>
 
-                    <Nav className='gap-2'>
+                        {user ? <Button onClick={handleLogOut} variant="secondary">Logout</Button>  :
+                            <Nav className='gap-2'>
                                 <Link className='text-black text-decoration-none' to={"/login"}>Login</Link>
                                 <Link className='text-black text-decoration-none' to={"/registration"}>
                                     Register
                                 </Link>
                             </Nav>
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
