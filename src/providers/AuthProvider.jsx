7cf3cbf18aaaf9836c10f39e69b1aca5cc61f51e
rabbyhasan <rabbyhasan4594@gmail.com>
2023-05-02 23:27:45 +0600

    loader added

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,7 +1,8 @@
 import React, { createContext } from 'react';
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
-
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export const AuthContext = createContext(null);
 
@@ -9,21 +10,38 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
     const signIn = (email, password) => {
         setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
-
-
     
     
+    
+
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, loggedUser => {
+            setUser(loggedUser);
+            setLoading(false);
+        })
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
 
     const authInfo = {
         user,
+        loading,
         createUser,
-        signIn,
+        logOut
     }
 
     return (
