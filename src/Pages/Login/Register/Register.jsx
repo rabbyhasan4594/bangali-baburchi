b4360a84dec08c41d142b0748d97aa4818c2ca33
rabbyhasan <rabbyhasan4594@gmail.com>
2023-05-02 22:01:42 +0600

    firebase config added

--- src/Pages/Login/Register/Register.jsx
+++ src/Pages/Login/Register/Register.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import NavigationBar from '../../../NavigationBar/NavigationBar';
 
 const Register = () => {
     return (
         <div>
-            
+            <NavigationBar></NavigationBar>
         </div>
     );
 };
