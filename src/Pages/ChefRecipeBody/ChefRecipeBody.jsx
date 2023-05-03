76f8ee007ccae80bff9dff4abc8d81bff807dacb
rabbyhasan <rabbyhasan4594@gmail.com>
2023-05-04 00:33:37 +0600

    chef recipe body added

---
+++ src/Pages/ChefRecipeBody/ChefRecipeBody.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button, Card, Container } from 'react-bootstrap';
+
+const ChefRecipeBody = ({ recipes }) => {
+    console.log(recipes)
+    const { name, cooking_method, ingredients, rating } = recipes;
+    return (
+        <div>
+            <Container className='p-5'>
+                <Card className='gap-3 mx-lg-5'>
+                    <Card.Header as="h4" className='text-center'>Bangali Baburchi</Card.Header>
+                    <Card.Body>
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Text>
+                            Ingredients : {ingredients}
+                        </Card.Text>
+                        <Card.Text>
+                            cooking_method : {cooking_method}
+                        </Card.Text>
+                        <Card.Text>
+                            Rating : {rating}
+                        </Card.Text>
+                        <Button variant="primary">Favorite button</Button>
+                    </Card.Body>
+                </Card>
+            </Container>
+        </div>
+    );
+};
+
+export default ChefRecipeBody;
--- src/Pages/ChefRecipes/ChefRecipes.jsx
+++ src/Pages/ChefRecipes/ChefRecipes.jsx
@@ -3,17 +3,26 @@ import NavigationBar from '../../NavigationBar/NavigationBar';
 import Footer from '../../Footer/Footer';
 import { useLoaderData, useParams } from 'react-router-dom';
 import ChefRecipeBanner from '../ChefRecipeBanner/ChefRecipeBanner';
+import ChefRecipeBody from '../ChefRecipeBody/ChefRecipeBody';
 
 const ChefRecipes = () => {
     const {id} = useParams();
     const ChefRecipes = useLoaderData();
     console.log(ChefRecipes)
+    const recipes=ChefRecipes.recipes;
     return (
         <div>
             <NavigationBar></NavigationBar>
             <ChefRecipeBanner 
              key={ChefRecipes.id}
              ChefRecipes={ChefRecipes} ></ChefRecipeBanner>
+             {
+                recipes.map(recipes => <ChefRecipeBody
+                    key={recipes.id}
+                    recipes={recipes}></ChefRecipeBody>)
+
+             }
+             
             <Footer></Footer>
         </div>
     );
