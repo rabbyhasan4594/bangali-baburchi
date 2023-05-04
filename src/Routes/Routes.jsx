7dad24ad88534cd6db8e86b41a19f0fbfd72aa1d
rabbyhasan <rabbyhasan4594@gmail.com>
2023-05-04 12:47:47 +0600

    error page added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,11 +6,13 @@ import Login from "../Pages/Login/Login/Login";
 import Register from "../Pages/Login/Register/Register";
 import ChefSection from "../Pages/ChefSection/ChefSection";
 import ChefRecipes from "../Pages/ChefRecipes/ChefRecipes";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Main></Main>,
+        errorElement: <ErrorPage />,
         children: [
             {
                 path: "/",
@@ -22,19 +24,24 @@ const router = createBrowserRouter([
     {
         path: "/blog",
         element: <Blog></Blog>,
+        errorElement: <ErrorPage />,
     },
     {
         path: "/login",
         element: <Login></Login>,
+        errorElement: <ErrorPage />,
     },
     {
         path: '/registration',
-        element: <Register></Register>
+        element: <Register></Register>,
+        errorElement: <ErrorPage />,
+
     },
     {
 
         path: '/chefRecipes/:id',
         element: <ChefRecipes />,
+        errorElement: <ErrorPage />,
         loader: ({params}) => fetch(`https://bangali-baburchi-server-rabbyhasan4594.vercel.app/chef/${params.id}`)
 
     }
